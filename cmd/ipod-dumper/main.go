59844c775ef85238e026754a7153ed7e81e98e60
Andrew Onyshchuk <andrew.onyshchuk@uptake.com>
2017-11-15 12:55:50 -0600

    cmd bugfixes

--- cmd/ipod-dumper/main.go
+++ cmd/ipod-dumper/main.go
@@ -19,33 +19,18 @@ func main() {
 		log.Fatal(err)
 	}
 
-	reportReader := hid.NewRawReportReader(f)
-	hidDecoder := hid.NewDecoderDefault(reportReader)
-	packetReader := ipod.NewTransportPacketReader(hidDecoder)
-
-	reportWriter := hid.NewRawReportWriter(f)
-	hidEncoder := hid.NewEncoderDefault(reportWriter)
-	packetWriter := ipod.NewTransportPacketWriter(hidEncoder)
+	reportReader, reportWriter := hid.NewRawReportReader(f), hid.NewRawReportWriter(f)
+	hidDecoder, hidEncoder := hid.NewDecoderDefault(reportReader), hid.NewEncoderDefault(reportWriter)
+	packetReader, packetWriter := ipod.NewTransportPacketReader(hidDecoder), ipod.NewTransportPacketWriter(hidEncoder)
 
 	devGeneral := &DevGeneral{}
 
 	packetRW := ipod.NewLoggingPacketReadWriter(packetReader, packetWriter, os.Stderr)
 
-	errCnt := 0
 	for {
-		//var fdr syscall.FdSet
-		//syscall.Select(int(f.Fd()+1), &fdr, nil, nil, &syscall.Timeval{Sec: 1})
 		packet, err := packetRW.ReadPacket()
 		if err != nil {
 			log.Printf("Error: %v", err)
-			//time.Sleep(10 * time.Millisecond)
-			errCnt++
-		} else {
-			errCnt = 0
-		}
-
-		if errCnt == 50 {
-			break
 		}
 		//log.Printf("err: %v packet go-syntax: %#v", err, packet)
 		//log.Printf("packet stringer: %v", packet)
