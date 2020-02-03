a65a218c652de80d481eafab42c0580c52ef1f85
Andrew Onyshchuk <andryk.rv@gmail.com>
2020-02-03 15:53:58 -0600

    Add GetiPodStateInfo/RetiPodStateInfo

--- lingo-dispremote/handler.go
+++ lingo-dispremote/handler.go
@@ -1,6 +1,9 @@
 package dispremote
 
 import (
+	"errors"
+	"time"
+
 	"github.com/oandrew/ipod"
 )
 
@@ -39,9 +42,65 @@ func HandleDispRemote(req *ipod.Command, tr ipod.CommandWriter, dev DeviceDispRe
 		})
 
 	case *GetiPodStateInfo:
-	// ipod.Respond(req, tr,  &RetiPodStateInfo{
-	// })
-	// todo
+		t := &RetiPodStateInfo{
+			InfoType: msg.InfoType,
+		}
+
+		switch msg.InfoType {
+		case InfoTypeTrackPositionMs:
+			t.InfoData = &InfoTrackPositionMs{TrackPositionMs: 0}
+		case InfoTypeTrackIndex:
+			t.InfoData = &InfoTrackIndex{TrackIndex: 1}
+		case InfoTypeChapterIndex:
+			t.InfoData = &InfoChapterIndex{
+				TrackIndex:   1,
+				ChapterCount: 0,
+				ChapterIndex: 0,
+			}
+		case InfoTypePlayStatus:
+			t.InfoData = &InfoPlayStatus{
+				PlayStatus: PlayStatusPlaying,
+			}
+		case InfoTypeVolume:
+			t.InfoData = &InfoVolume{MuteState: 0x00, UIVolumeLevel: 255}
+		case InfoTypePower:
+			t.InfoData = &InfoPower{PowerState: 0x05, BatteryLevel: 255}
+		case InfoTypeEqualizer:
+			t.InfoData = &InfoEqualizer{0x00}
+		case InfoTypeShuffle:
+			t.InfoData = &InfoShuffle{0x00}
+		case InfoTypeRepeat:
+			t.InfoData = &InfoRepeat{0x00}
+		case InfoTypeDateTime:
+			d := time.Now()
+			t.InfoData = &InfoDateTime{
+				Year:   uint16(d.Year()),
+				Month:  uint8(d.Month()),
+				Day:    uint8(d.Day()),
+				Hour:   uint8(d.Hour()),
+				Minute: uint8(d.Minute()),
+			}
+		case InfoTypeBacklight:
+			t.InfoData = &InfoBacklight{BacklightLevel: 255}
+		case InfoTypeHoldSwitch:
+			t.InfoData = &InfoHoldSwitch{HoldSwitchState: 0x00}
+		case InfoTypeSoundCheck:
+			t.InfoData = &InfoSoundCheck{SoundCheckState: 0x00}
+		case InfoTypeAudiobookSpeed:
+			t.InfoData = &InfoAudiobookSpeed{0x00}
+		case InfoTypeTrackPositionSec:
+			t.InfoData = &InfoTrackPositionSec{0}
+		case InfoTypeVolume2:
+			t.InfoData = &InfoVolume2{
+				MuteState:           0x00,
+				UIVolumeLevel:       255,
+				AbsoluteVolumeLevel: 255,
+			}
+		default:
+			return errors.New("unknown info type")
+		}
+
+		ipod.Respond(req, tr, t)
 
 	case *SetiPodStateInfo:
 		ipod.Respond(req, tr, ackSuccess(req))
